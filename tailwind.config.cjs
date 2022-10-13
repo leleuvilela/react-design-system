214b5518b60e17ec53f824806b7843879316a996
Vinicius Vilela Nunes <ext.vinicius.nunes@farfetch.com>
2022-10-13 01:26:49 +0100

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    colors: {
+      black: "#000000",
+      white: "#ffffff",
+      gray: {
+        900: "#121214",
+        800: "#202024",
+        400: "#7C7C8A",
+        100: "#E1E1E6",
+      },
+      cyan: {
+        500: "#81D8F7",
+        300: "#A5E4FB",
+      },
+      transparent: "transparent",
+    },
+    extend: {
+      fontFamily: {
+        sans: "Inter, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
